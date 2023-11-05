beb23cf222eb87151d635530cd61dfed32483a3c
Elsa Frankel <frankelelsa@gmail.com>
2023-11-05 10:21:20 -0500

    -m send help

---
+++ main.js
@@ -0,0 +1,10 @@
+function scrollToButtons() {
+  // Scrolls to the "Host Event" or "Browse" buttons
+  window.scrollTo(401, 700);
+}
+
+function goToPostForm() {
+  // Scrolls to the post form after "Host Event" button is clicked
+  document.getElementById("post-form-container").style.display = "flex";
+  window.scrollTo(401, 1350);
+}
